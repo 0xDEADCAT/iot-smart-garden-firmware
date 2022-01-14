c54c69411bad0c11ea1ad55b8deaf844edb4aa9c
Dawid Osuchowski <dawiosu469@student.polsl.pl>
2022-01-14 04:38:50 +0100

    Update button GPIO in board_pump.h

--- include/board_pump.h
+++ include/board_pump.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define SMARTGARDEN_BOARD_BUTTON_GPIO          18      /* This is the button that is used for toggling the output */
+#define SMARTGARDEN_BOARD_BUTTON_GPIO          0      /* This is the button that is used for toggling the output */
 #define SMARTGARDEN_BOARD_BUTTON_ACTIVE_LEVEL  0
 #define SMARTGARDEN_BOARD_OUTPUT_GPIO          2     /* This is the GPIO on which the output will be set */
 #define SMARTGARDEN_BOARD_PUMP_GPIO            33     /* This is the GPIO through which the pump will be controlled */
